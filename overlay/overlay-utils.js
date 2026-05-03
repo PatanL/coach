830c001d6ea52d6ec5c1dd9ff3456391920dcdaa
pecies <patboss63@gmail.com>
2026-05-03 11:31:37 -0700

    overlay: ignore global Escape while typing

--- overlay/overlay-utils.js
+++ overlay/overlay-utils.js
@@ -40,9 +40,18 @@
     return isTextInputTarget(t) || isInteractiveTarget(t);
   }
 
+  // Escape should never trigger global overlay behavior while the user is typing.
+  // We keep it narrower than Enter: if focus is on a button/link, Escape can still be a reasonable
+  // "back/cancel" affordance.
+  function shouldIgnoreGlobalEscape(target) {
+    const t = findHotkeyRelevantTarget(target);
+    return isTextInputTarget(t);
+  }
+
   return {
     isTextInputTarget,
     isInteractiveTarget,
-    shouldIgnoreGlobalEnter
+    shouldIgnoreGlobalEnter,
+    shouldIgnoreGlobalEscape
   };
 });
