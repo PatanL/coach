bbdacf3b189e2749616c88fb41bcda025bbd5e1b
pecies <patboss63@gmail.com>
2026-05-04 04:03:20 -0700

    test(overlay): cover DRIFT_PERSIST global Enter rule

--- overlay/overlay-utils.test.js
+++ overlay/overlay-utils.test.js
@@ -47,16 +47,14 @@ test("shouldIgnoreGlobalEnter: child of button/link should still block global En
   assert.equal(shouldIgnoreGlobalEnter(spanInsideButton), true);
 });
 
-test("shouldAllowGlobalEnter: DRIFT_PERSIST disables global Enter", () => {
+test("shouldAllowGlobalEnter: DRIFT_PERSIST requires explicit click", () => {
   assert.equal(shouldAllowGlobalEnter("DRIFT_PERSIST"), false);
   assert.equal(shouldAllowGlobalEnter("drift_persist"), false);
+});
+
+test("shouldAllowGlobalEnter: other events allow global Enter", () => {
   assert.equal(shouldAllowGlobalEnter("DRIFT_START"), true);
+  assert.equal(shouldAllowGlobalEnter(""), true);
   assert.equal(shouldAllowGlobalEnter(null), true);
 });
 
-test("shouldAllowGlobalEnter: DRIFT_PERSIST requires deliberate click", () => {
-  assert.equal(shouldAllowGlobalEnter("DRIFT_PERSIST"), false);
-  assert.equal(shouldAllowGlobalEnter("drift_persist"), false);
-  assert.equal(shouldAllowGlobalEnter("DRIFT_START"), true);
-  assert.equal(shouldAllowGlobalEnter(""), true);
-});
