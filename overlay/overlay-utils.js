5fc9fa5a631cef8da2386b0e372d561b37863f74
pecies <patboss63@gmail.com>
2026-05-16 17:31:29 -0700

    overlay: ignore global Enter for ARIA inputs via closest()

--- overlay/overlay-utils.js
+++ overlay/overlay-utils.js
@@ -35,7 +35,7 @@
     // element that should "own" the keyboard interaction.
     if (typeof target.closest === "function") {
       const hit = target.closest(
-        'input,textarea,select,[contenteditable="true"],button,a,[role="button"],[role="link"]'
+        'input,textarea,select,[contenteditable="true"],[role="textbox"],[role="searchbox"],[role="combobox"],button,a,[role="button"],[role="link"]'
       );
       if (hit) return hit;
     }
